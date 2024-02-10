b0ed4da1d941218724c6589ab3ccfae7608e3ecf
Drowsy_Fox03 <francesco.volpini2@studio.unibo.it>
2024-02-10 18:35:33 +0100

    Inizio implementazione menu a scelta

---
+++ src/main/java/menu/Caratteristiche.java
@@ -0,0 +1,12 @@
+package menu;
+
+import java.util.List;
+
+public class Caratteristiche {
+    static List<String> MacroTratti = List.of("CAPELLI", "OCCHI", "NASO", "BOCCA");
+    static List<String> capelli = List.of("BIONDI", "CASTANI", "NERI", "ROSSI", "LUNGHI", "CORTI", "CALVO", "STEMPIATO");
+    static List<String> Occhi = List.of("AZZURRI", "VERDI", "NOCCIOLA", "OCCHIALI");
+    static List<String> Naso = List.of("PICCOLO", "SCHIACCIATO", "A PATATA", "ADUNCO");
+    static List<String> Bocca = List.of("APERTA", "CHIUSA", "SORRIDENTE", "TRISTE", "BARBA");
+
+}
---
+++ src/main/java/menu/menu.java
@@ -0,0 +1,10 @@
+package menu;
+
+public class menu {
+
+
+ 
+
+
+
+}
