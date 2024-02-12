47ab64ada3e0d16f90eb9da138b498205c9a4d32
Alex Petreti <alex.petreti@studio.unibo.it>
2024-02-12 13:52:38 +0100

    molti errori di stile corretti e aggiunta di una classe test

---
+++ src/main/java/schermataFinale/SchermataFinaleTest.java
@@ -0,0 +1,12 @@
+package schermatafinale;
+
+public class SchermataFinaleTest {
+    /**
+     * Metodo principale per avviare il programma di test.
+     * @param args
+     * @throws java.io.IOException
+     */
+    public static void main(final String[] args) throws java.io.IOException {
+        new SchermataFinaleGUI();
+    }
+}
