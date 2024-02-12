f4da7355bd1bd9c0a23f9cde4177f388d6c18072
Drowsy_Fox03 <francesco.volpini2@studio.unibo.it>
2024-02-13 00:21:38 +0100

    correzione commit precedente

--- src/main/java/menu/menuPopup.java
+++ src/main/java/menu/menuPopup.java
@@ -1,7 +1,8 @@
 package menu;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.Dimension;
+import java.awt.BorderLayout;
 
 
 /*
@@ -9,40 +10,48 @@ import java.awt.*;
  * delle caratteristiche
  */
 
-public class menuPopup {
+public class MenuPopup {
 
-    public static void main(String[] args) {
+    public void menu() {
 
-        /*
-         * Crea la finestra che ospiterà il menu
-         */
-        final JFrame frame = new JFrame();
-        frame.setPreferredSize(new Dimension(1000, 500));
-        final JToolBar toolBar = new JToolBar();
-
-        /*
-         * Crea i pulsanti che comporranno
-         * il menu popup
-         */
-        
-        Pulsanti p = new Pulsanti();
-        p.Pulsante(frame, toolBar, "GENERE", caratteristiche.Genere);
-        p.Pulsante(frame, toolBar, "HA I CAPELLI?", caratteristiche.yesORno);
-        p.Pulsante(frame, toolBar, "COLORE CAPELLI", caratteristiche.coloreCapelli);
-        p.Pulsante(frame, toolBar, "TIPO DI CAPELLI", caratteristiche.tipiCapelli);
-        p.Pulsante(frame, toolBar, "LUNGHEZZA CAPELLI", caratteristiche.lunghezzaCapelli);
-        p.Pulsante(frame, toolBar, "COLORE OCCHI", caratteristiche.coloreOcchi);
-        p.Pulsante(frame, toolBar, "HA LA BARBA?", caratteristiche.yesORno);
-        p.Pulsante(frame, toolBar, "HA I BAFFI?", caratteristiche.yesORno);
-        p.Pulsante(frame, toolBar, "HA ACCESSORI?", caratteristiche.yesORno);
-        p.Pulsante(frame, toolBar, "HA OCCHIALI?", caratteristiche.yesORno);
-        
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                /*
+                * Crea la finestra che ospiterà il menu
+                */
+                final JFrame frame = new JFrame();
+                frame.setPreferredSize(new Dimension(1000, 500));
+                final JToolBar toolBar = new JToolBar();
         
 
-        frame.getContentPane().add(toolBar, BorderLayout.NORTH);
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+                /*
+                * Crea i pulsanti che comporranno
+                * il menu popup
+                */
+        
+                final Pulsanti p = new Pulsanti();
+                p.pulsante(frame, toolBar, "GENERE", CaratteristicheUtil.GENERE);
+                p.pulsante(frame, toolBar, "HA I CAPELLI?", CaratteristicheUtil.YESORNO);
+                p.pulsante(frame, toolBar, "COLORE CAPELLI", CaratteristicheUtil.COLORECAPELLI);
+                p.pulsante(frame, toolBar, "TIPO DI CAPELLI", CaratteristicheUtil.TIPICAPELLI);
+                p.pulsante(frame, toolBar, "LUNGHEZZA CAPELLI", CaratteristicheUtil.LUNGHEZZACAPELLI);
+                p.pulsante(frame, toolBar, "COLORE OCCHI", CaratteristicheUtil.COLOREOCCHI);
+                p.pulsante(frame, toolBar, "HA LA BARBA?", CaratteristicheUtil.YESORNO);
+                p.pulsante(frame, toolBar, "HA I BAFFI?", CaratteristicheUtil.YESORNO);
+                p.pulsante(frame, toolBar, "HA ACCESSORI?", CaratteristicheUtil.YESORNO);
+                p.pulsante(frame, toolBar, "HA OCCHIALI?", CaratteristicheUtil.YESORNO);
+                
+                
+
+                frame.getContentPane().add(toolBar, BorderLayout.NORTH);
+                frame.pack();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+            }
+        });
+    
+
     }
 }
