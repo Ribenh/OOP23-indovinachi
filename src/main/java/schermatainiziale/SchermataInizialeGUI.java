a26ce6f49ade2f2abb1ac83cfd8144bbb4b5b42e
Alex Petreti <alex.petreti@studio.unibo.it>
2024-02-16 20:01:16 +0100

    correzione errori

--- src/main/java/schermatainiziale/SchermataInizialeGUI.java
+++ src/main/java/schermatainiziale/SchermataInizialeGUI.java
@@ -1,16 +1,8 @@
 package schermatainiziale;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 /**
  * Interfaccia grafica della schermata di selezione della modalità.
