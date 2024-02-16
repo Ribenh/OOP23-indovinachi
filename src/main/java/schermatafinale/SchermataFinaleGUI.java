ef10fe784cfbe9ba3a538d9eb3d565443c83c10b
Alex Petreti <alex.petreti@studio.unibo.it>
2024-02-16 20:08:29 +0100

    correzione errori checkstyle

--- src/main/java/schermatafinale/SchermataFinaleGUI.java
+++ src/main/java/schermatafinale/SchermataFinaleGUI.java
@@ -1,8 +1,20 @@
 package schermatafinale;
 
-import javax.swing.*;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
-import java.awt.*;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
