e8a47eaae68e8fe33c1609973b6b4fd51a272bac
MircoVentaloro <mirco.ventaloro@studio.unibo.it>
2024-02-18 18:18:12 +0100

    Correzione ripetizione stringhe + correzione capelli da marroni a castani

--- src/main/java/personaggi/PersonaggiCreati.java
+++ src/main/java/personaggi/PersonaggiCreati.java
@@ -5,70 +5,112 @@ import java.util.List;
 import javax.swing.ImageIcon;
 import java.util.Collections;
 
-public class PersonaggiCreati {
-    // Lista dei personaggi disponibili
-    private static final List<Personaggio> personaggi = new ArrayList<>();
 
+
+/**
+ * Classe che gestisce la creazione dei personaggi disponibili nel gioco.
+ */
+public final class PersonaggiCreati {
+    // Lista dei personaggi disponibili
+    private static final List<Personaggio> PERSONAGGI = new ArrayList<>();
+    private static final String CORTI="Corti";
+    private static final String LISCI="Lisci";
+    private static final String MARRONI="Marroni";
+    private static final String CASTANI="Castani";
+    private static final String LUNGHI="Lunghi";
+    private static final String VERDI="Verdi";
     static {
         // Salvataggio immagini
-        ImageIcon basilIcon = new ImageIcon("immagini/basil.jpg");
-        ImageIcon melvinIcon = new ImageIcon("immagini/melvin.jpg");
-        ImageIcon hannahIcon = new ImageIcon("immagini/hannah.jpg");
-        ImageIcon simoneIcon = new ImageIcon("immagini/simone.jpg");
-        ImageIcon ianIcon = new ImageIcon("immagini/ian.jpg");
-        ImageIcon islaIcon = new ImageIcon("immagini/isla.jpg");
-        ImageIcon rupertIcon = new ImageIcon("immagini/rupert.jpg");
-        ImageIcon maggieIcon = new ImageIcon("immagini/maggie.jpg");
-        ImageIcon susanIcon = new ImageIcon("immagini/susan.jpg");
-        ImageIcon natalieIcon = new ImageIcon("immagini/natalie.jpg");
-        ImageIcon kimIcon = new ImageIcon("immagini/kim.jpg");
-        ImageIcon stephenIcon = new ImageIcon("immagini/stephen.jpg");
-        ImageIcon joshuaIcon = new ImageIcon("immagini/joshua.jpg");
-        ImageIcon xiaoMeiIcon = new ImageIcon("immagini/xiao mei.jpg");
-        ImageIcon jenniferIcon = new ImageIcon("immagini/jennifer.jpg");
-        ImageIcon brianIcon = new ImageIcon("immagini/brian.jpg");
-        ImageIcon garyIcon = new ImageIcon("immagini/gary.jpg");
-        ImageIcon martineIcon = new ImageIcon("immagini/martine.jpg");
-        ImageIcon billIcon = new ImageIcon("immagini/bill.jpg");
-        ImageIcon royIcon = new ImageIcon("immagini/roy.jpg");
-        ImageIcon ednaIcon = new ImageIcon("immagini/edna.jpg");
-        ImageIcon moIcon = new ImageIcon("immagini/mo.jpg");
-        ImageIcon kellyIcon = new ImageIcon("immagini/kelly.jpg");
-        ImageIcon peteIcon = new ImageIcon("immagini/pete.jpg");
-        
+        final ImageIcon basilIcon = new ImageIcon("immagini/basil.jpg");
+        final ImageIcon melvinIcon = new ImageIcon("immagini/melvin.jpg");
+        final ImageIcon hannahIcon = new ImageIcon("immagini/hannah.jpg");
+        final ImageIcon simoneIcon = new ImageIcon("immagini/simone.jpg");
+        final ImageIcon ianIcon = new ImageIcon("immagini/ian.jpg");
+        final ImageIcon islaIcon = new ImageIcon("immagini/isla.jpg");
+        final ImageIcon rupertIcon = new ImageIcon("immagini/rupert.jpg");
+        final ImageIcon maggieIcon = new ImageIcon("immagini/maggie.jpg");
+        final ImageIcon susanIcon = new ImageIcon("immagini/susan.jpg");
+        final ImageIcon natalieIcon = new ImageIcon("immagini/natalie.jpg");
+        final ImageIcon kimIcon = new ImageIcon("immagini/kim.jpg");
+        final ImageIcon stephenIcon = new ImageIcon("immagini/stephen.jpg");
+        final ImageIcon joshuaIcon = new ImageIcon("immagini/joshua.jpg");
+        final ImageIcon xiaoMeiIcon = new ImageIcon("immagini/xiao mei.jpg");
+        final ImageIcon jenniferIcon = new ImageIcon("immagini/jennifer.jpg");
+        final ImageIcon brianIcon = new ImageIcon("immagini/brian.jpg");
+        final ImageIcon garyIcon = new ImageIcon("immagini/gary.jpg");
+        final ImageIcon martineIcon = new ImageIcon("immagini/martine.jpg");
+        final ImageIcon billIcon = new ImageIcon("immagini/bill.jpg");
+        final ImageIcon royIcon = new ImageIcon("immagini/roy.jpg");
+        final ImageIcon ednaIcon = new ImageIcon("immagini/edna.jpg");
+        final ImageIcon moIcon = new ImageIcon("immagini/mo.jpg");
+        final ImageIcon kellyIcon = new ImageIcon("immagini/kelly.jpg");
+        final ImageIcon peteIcon = new ImageIcon("immagini/pete.jpg");
+
         // Creazione dei personaggi
-        personaggi.add(new PersonaggioImpl("Basil", basilIcon, true, "Neri", "Lisci", "Corti", "Marroni", false, false, false, true, null));
-        personaggi.add(new PersonaggioImpl("Melvin", melvinIcon, true, "Castani", "Lisci", "Corti", "Marroni", false, true, false, false, true));
-        personaggi.add(new PersonaggioImpl("Hannah", hannahIcon, true, "Castani", "Lisci", "Lunghi", "Marroni", false, false, true, false, false));
-        personaggi.add(new PersonaggioImpl("Simone", simoneIcon, true, "Castani", "Lisci", "Lunghi", "Verdi", false, false, false, false, false));
-        personaggi.add(new PersonaggioImpl("Ian", ianIcon, true, "Bianchi", "Lisci", "Corti", "Marroni", false, true, false, true, true));
-        personaggi.add(new PersonaggioImpl("Isla", islaIcon, true, "Arancioni", "Lisci", "Lunghi", "Verdi", false, false, false, false, false));
-        personaggi.add(new PersonaggioImpl("Rupert", rupertIcon, false, "Verdi", false, true, false, true, true));
-        personaggi.add(new PersonaggioImpl("Maggie", maggieIcon, true, "Castani", "Mossi", "Lunghi", "Marroni", false, false, false, false, false));
-        personaggi.add(new PersonaggioImpl("Susan", susanIcon, true, "Castani", "Ricci", "Lunghi", "Marroni", false, false, true, false, false));
-        personaggi.add(new PersonaggioImpl("Natalie", natalieIcon, true, "Biondi", "Lisci", "Lunghi", "Verdi", false, false, false, false, false));
-        personaggi.add(new PersonaggioImpl("Kim", kimIcon, true, "Castani", "Lisci", "Lunghi", "Marroni", false, true, false, false, false));
-        personaggi.add(new PersonaggioImpl("Stephen", stephenIcon, true, "Neri", "Lisci", "Corti", "Marroni", false, false, false, false, true));
-        personaggi.add(new PersonaggioImpl("Joshua", joshuaIcon, true, "Castani", "Lisci", "Corti", "Marroni", false, false, false, false, true));
-        personaggi.add(new PersonaggioImpl("Natalie", natalieIcon, true, "Biondi", "Lisci", "Lunghi", "Verdi", false, false, false, false, false));
-        personaggi.add(new PersonaggioImpl("Xiao Mei", xiaoMeiIcon, true, "Neri", "Lisci", "Lunghi", "Marroni", false, false, false, false, false));
-        personaggi.add(new PersonaggioImpl("Jennifer", jenniferIcon, true, "Arancioni", "Lisci", "Lunghi", "Marroni", false, false, false, false, false));
-        personaggi.add(new PersonaggioImpl("Brian", brianIcon, true, "Castani", "Lisci", "Corti", "Verdi", true, true, false, true, true));
-        personaggi.add(new PersonaggioImpl("Gary", garyIcon, true, "Castani", "Lisci", "Corti", "Verdi", false, false, false, false, true));
-        personaggi.add(new PersonaggioImpl("Martine", martineIcon, true, "Castani", "Lisci", "Lunghi", "Marroni", false, false, false, false, false));
-        personaggi.add(new PersonaggioImpl("Bill", billIcon, true, "Castani", "Lisci", "Corti", "Marroni", true, false, false, true, true));
-        personaggi.add(new PersonaggioImpl("Roy", royIcon, true, "Castani", "Lisci", "Corti", "Marroni", false, true, false, false, true));
-        personaggi.add(new PersonaggioImpl("Edna", ednaIcon, true, "Bianchi", "Lisci", "Lunghi", "Azzurri", false, true, false, false, false));
-        personaggi.add(new PersonaggioImpl("Mo", moIcon, false, "Marroni", true, false, true, true, true));
-        personaggi.add(new PersonaggioImpl("Kelly", kellyIcon, true, "Biondi", "Lisci", "Lunghi", "Verdi", false, false, false, false, false));
-        personaggi.add(new PersonaggioImpl("Pete", peteIcon, true, "Marroni", "Ricci", "Corti", "Marroni", true, false, false, true, true));
+        PERSONAGGI.add(new PersonaggioImpl("Basil", basilIcon, true, "Neri", LISCI, CORTI, MARRONI, false, 
+        false, false, true, null));
+        PERSONAGGI.add(new PersonaggioImpl("Melvin", melvinIcon, true, CASTANI, LISCI, CORTI, MARRONI, false, 
+        true, false, false, true));
+        PERSONAGGI.add(new PersonaggioImpl("Hannah", hannahIcon, true, CASTANI, LISCI, LUNGHI, MARRONI, false, 
+        false, true, false, false));
+        PERSONAGGI.add(new PersonaggioImpl("Simone", simoneIcon, true, CASTANI, LISCI, LUNGHI, VERDI, false, 
+        false, false, false, false));
+        PERSONAGGI.add(new PersonaggioImpl("Ian", ianIcon, true, "Bianchi", LISCI, CORTI, MARRONI, false, 
+        true, false, true, true));
+        PERSONAGGI.add(new PersonaggioImpl("Isla", islaIcon, true, "Arancioni", LISCI, LUNGHI, VERDI, false, 
+        false, false, false, false));
+        PERSONAGGI.add(new PersonaggioImpl("Rupert", rupertIcon, false, VERDI, false, true, false, true, true));
+        PERSONAGGI.add(new PersonaggioImpl("Maggie", maggieIcon, true, CASTANI, "Mossi", LUNGHI, MARRONI, false, 
+        false, false, false, false));
+        PERSONAGGI.add(new PersonaggioImpl("Susan", susanIcon, true, CASTANI, "Ricci", LUNGHI, MARRONI, false, 
+        false, true, false, false));
+        PERSONAGGI.add(new PersonaggioImpl("Kim", kimIcon, true, CASTANI, LISCI, LUNGHI, MARRONI, false, 
+        true, false, false, false));
+        PERSONAGGI.add(new PersonaggioImpl("Stephen", stephenIcon, true, "Neri", LISCI, CORTI, MARRONI, false, 
+        false, false, false, true));
+        PERSONAGGI.add(new PersonaggioImpl("Joshua", joshuaIcon, true, CASTANI, LISCI, CORTI, MARRONI, false, 
+        false, false, false, true));
+        PERSONAGGI.add(new PersonaggioImpl("Natalie", natalieIcon, true, "Biondi", LISCI, LUNGHI, VERDI, false, 
+        false, false, false, false));
+        PERSONAGGI.add(new PersonaggioImpl("Xiao Mei", xiaoMeiIcon, true, "Neri", LISCI, LUNGHI, MARRONI, false, 
+        false, false, false, false));
+        PERSONAGGI.add(new PersonaggioImpl("Jennifer", jenniferIcon, true, "Arancioni", LISCI, LUNGHI, MARRONI, false, 
+        false, false, false, false));
+        PERSONAGGI.add(new PersonaggioImpl("Brian", brianIcon, true, CASTANI, LISCI, CORTI, VERDI, true, 
+        true, false, true, true));
+        PERSONAGGI.add(new PersonaggioImpl("Gary", garyIcon, true, CASTANI, LISCI, CORTI, VERDI, false, 
+        false, false, false, true));
+        PERSONAGGI.add(new PersonaggioImpl("Martine", martineIcon, true, CASTANI, LISCI, LUNGHI, MARRONI, false, 
+        false, false, false, false));
+        PERSONAGGI.add(new PersonaggioImpl("Bill", billIcon, true, CASTANI, LISCI, CORTI, MARRONI, true, 
+        false, false, true, true));
+        PERSONAGGI.add(new PersonaggioImpl("Roy", royIcon, true, CASTANI, LISCI, CORTI, MARRONI, false, 
+        true, false, false, true));
+        PERSONAGGI.add(new PersonaggioImpl("Edna", ednaIcon, true, "Bianchi", LISCI, LUNGHI, "Azzurri", false, 
+        true, false, false, false));
+        PERSONAGGI.add(new PersonaggioImpl("Mo", moIcon, false, MARRONI, true, false, true, true, true));
+        PERSONAGGI.add(new PersonaggioImpl("Kelly", kellyIcon, true, "Biondi", LISCI, LUNGHI, VERDI, false, 
+        false, false, false, false));
+        PERSONAGGI.add(new PersonaggioImpl("Pete", peteIcon, true, MARRONI, "Ricci", CORTI, MARRONI, true, 
+        false, false, true, true));
     }
 
+    /**
+     * Restituisce una lista di personaggi creata mescolando casualmente i personaggi disponibili.
+     * @return Una lista di personaggi.
+     */
     public static List<Personaggio> creaPersonaggi() {
         // Copia la lista dei personaggi disponibili
-        List<Personaggio> personaggiRandom = new ArrayList<>(personaggi);
+        List<Personaggio> personaggiRandom = new ArrayList<>(PERSONAGGI);
         // Mescola la lista in modo casuale
         Collections.shuffle(personaggiRandom);
         return personaggiRandom;
+    }
+
+    /**
+     * Costruttore privato per prevenire l'istanziazione esterna.
+     */
+    private PersonaggiCreati() {
+        throw new AssertionError("Classe di utilità non deve essere istanziata");
     }
 }
--- src/main/java/personaggi/PersonaggioImpl.java
+++ src/main/java/personaggi/PersonaggioImpl.java
@@ -2,10 +2,10 @@ package personaggi;
 
 import javax.swing.ImageIcon;
 
-import java.util.Optional;
-
+/**
+ * Implementazione dell'interfaccia {@link Personaggio} che definisce le caratteristiche di un personaggio del gioco.
+ */
 public class PersonaggioImpl implements Personaggio {
-
     private String nome;
     private ImageIcon immagine;
     private Boolean hasCapelli;
@@ -19,12 +19,32 @@ public class PersonaggioImpl implements Personaggio {
     private Boolean baffi;
     private Boolean uomo;
 
+    /**
+     * Costruttore vuoto di default.
+     */
     public PersonaggioImpl() {
     }
 
-    public PersonaggioImpl(String nome, ImageIcon immagine, Boolean hasCapelli, String coloreCapelli, String tipologiaCapelli, String lunghezzaCapelli, String coloreOcchi, Boolean barba, Boolean occhiali, Boolean accessorio, Boolean baffi, Boolean uomo) {
+    /**
+    * Costruttore per creare un personaggio con tutte le caratteristiche definite.
+    * @param nome Il nome del personaggio.
+    * @param immagine L'immagine del personaggio.
+    * @param hasCapelli True se il personaggio ha i capelli, altrimenti False.
+    * @param coloreCapelli Il colore dei capelli del personaggio.
+    * @param tipologiaCapelli La tipologia dei capelli del personaggio.
+    * @param lunghezzaCapelli La lunghezza dei capelli del personaggio.
+    * @param coloreOcchi Il colore degli occhi del personaggio.
+    * @param barba True se il personaggio ha la barba, altrimenti False.
+    * @param occhiali True se il personaggio indossa gli occhiali, altrimenti False.
+    * @param accessorio True se il personaggio ha accessori, altrimenti False.
+    * @param baffi True se il personaggio ha i baffi, altrimenti False.
+    * @param uomo True se il personaggio è di sesso maschile, altrimenti False.
+    */
+    public PersonaggioImpl(final String nome, final ImageIcon immagine, final Boolean hasCapelli, final String coloreCapelli, 
+    final String tipologiaCapelli, final String lunghezzaCapelli, final String coloreOcchi, final Boolean barba, 
+    final Boolean occhiali, final Boolean accessorio, final Boolean baffi, final Boolean uomo) {
         this.nome = nome;
-        this.immagine = immagine;
+        this.immagine = new ImageIcon(immagine.getImage());
         this.hasCapelli = hasCapelli;
         this.coloreCapelli = coloreCapelli;
         this.tipologiaCapelli = tipologiaCapelli;
@@ -36,9 +56,22 @@ public class PersonaggioImpl implements Personaggio {
         this.uomo = uomo;
     }
 
-    public PersonaggioImpl(String nome, ImageIcon immagine, Boolean hasCapelli, String coloreOcchi, Boolean barba, Boolean occhiali, Boolean accessorio, Boolean baffi, Boolean uomo) {
+    /**
+     * Costruttore per creare un personaggio con caratteristiche parziali.
+     * @param nome Il nome del personaggio.
+     * @param immagine L'immagine del personaggio.
+     * @param hasCapelli True se il personaggio ha i capelli, altrimenti False.
+     * @param coloreOcchi Il colore degli occhi del personaggio.
+     * @param barba True se il personaggio ha la barba, altrimenti False.
+     * @param occhiali True se il personaggio indossa gli occhiali, altrimenti False.
+     * @param accessorio True se il personaggio ha accessori, altrimenti False.
+     * @param baffi True se il personaggio ha i baffi, altrimenti False.
+     * @param uomo True se il personaggio è di sesso maschile, altrimenti False.
+     */
+    public PersonaggioImpl(final String nome, final ImageIcon immagine, final Boolean hasCapelli, final String coloreOcchi, 
+    final Boolean barba, final Boolean occhiali, final Boolean accessorio, final Boolean baffi, final Boolean uomo) {
         this.nome = nome;
-        this.immagine = immagine;
+        this.immagine = new ImageIcon(immagine.getImage());
         this.hasCapelli = hasCapelli;
         this.coloreOcchi = coloreOcchi;
         this.barba = barba;
@@ -47,123 +80,228 @@ public class PersonaggioImpl implements Personaggio {
         this.uomo = uomo;
     }
 
+    // Implementazione dei metodi dell'interfaccia Personaggio
+    /**
+     * Restituisce il nome del personaggio.
+     * @return Il nome del personaggio.
+     */
     @Override
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Imposta il nome del personaggio.
+     * @param nome Il nuovo nome del personaggio.
+     */
     @Override
-    public void setNome(String nome) {
+    public void setNome(final String nome) {
         this.nome = nome;
     }
 
+    /**
+    * Restituisce un'istanza di ImageIcon che rappresenta l'immagine del personaggio.
+    * Nota: Questo metodo restituisce una copia dell'immagine anziché l'oggetto originale
+    * per proteggere la rappresentazione interna della classe da modifiche esterne.
+    * Modifiche all'immagine restituita non influenzeranno l'immagine interna della classe.
+    * @return Una copia dell'immagine del personaggio.
+    */
     @Override
     public ImageIcon getImmagine() {
-        return immagine;
+        // Restituisce una copia dell'immagine
+        return new ImageIcon(immagine.getImage());
     }
 
+    /**
+    * Imposta l'immagine del personaggio.
+    * Nota: Questo metodo accetta una copia dell'immagine anziché l'oggetto originale
+    * per proteggere la rappresentazione interna della classe da modifiche esterne.
+    * Modifiche all'immagine fornita non influenzeranno l'immagine interna della classe.
+    * @param immagine Una copia dell'immagine da impostare per il personaggio.
+    */
     @Override
-    public void setImmagine(ImageIcon immagine) {
-        this.immagine = immagine;
+    public void setImmagine(final ImageIcon immagine) {
+        // Imposta una copia dell'immagine anziché l'oggetto originale
+        this.immagine = new ImageIcon(immagine.getImage());
     }
 
+    /**
+     * Verifica se il personaggio ha i capelli.
+     * @return True se il personaggio ha i capelli, altrimenti False.
+     */
     @Override
     public Boolean hasCapelli() {
         return hasCapelli;
     }
 
+    /**
+     * Imposta se il personaggio ha i capelli.
+     * @param hasCapelli True se il personaggio ha i capelli, altrimenti False.
+     */
     @Override
-    public void setHasCapelli(Boolean hasCapelli) {
+    public void setHasCapelli(final Boolean hasCapelli) {
         this.hasCapelli = hasCapelli;
     }
 
+    /**
+     * Restituisce il colore dei capelli del personaggio.
+     * @return Il colore dei capelli del personaggio.
+     */
     @Override
     public String getColoreCapelli() {
         return coloreCapelli;
     }
 
+    /**
+     * Imposta il colore dei capelli del personaggio.
+     * @param coloreCapelli Il nuovo colore dei capelli del personaggio.
+     */
     @Override
-    public void setColoreCapelli(String coloreCapelli) {
+    public void setColoreCapelli(final String coloreCapelli) {
         this.coloreCapelli = coloreCapelli;
     }
 
+    /**
+     * Restituisce la tipologia dei capelli del personaggio.
+     * @return La tipologia dei capelli del personaggio.
+     */
     @Override
     public String getTipologiaCapelli() {
         return tipologiaCapelli;
     }
 
+    /**
+     * Imposta la tipologia dei capelli del personaggio.
+     * @param tipologiaCapelli La nuova tipologia dei capelli del personaggio.
+     */
     @Override
-    public void setTipologiaCapelli(String tipologiaCapelli) {
+    public void setTipologiaCapelli(final String tipologiaCapelli) {
         this.tipologiaCapelli = tipologiaCapelli;
     }
 
+    /**
+     * Restituisce la lunghezza dei capelli del personaggio.
+     * @return La lunghezza dei capelli del personaggio.
+     */
     @Override
     public String getLunghezzaCapelli() {
         return lunghezzaCapelli;
     }
 
+    /**
+     * Imposta la lunghezza dei capelli del personaggio.
+     * @param lunghezzaCapelli La nuova lunghezza dei capelli del personaggio.
+     */
     @Override
-    public void setLunghezzaCapelli(String lunghezzaCapelli) {
+    public void setLunghezzaCapelli(final String lunghezzaCapelli) {
         this.lunghezzaCapelli = lunghezzaCapelli;
     }
 
+    /**
+     * Restituisce il colore degli occhi del personaggio.
+     * @return Il colore degli occhi del personaggio.
+     */
     @Override
     public String getColoreOcchi() {
         return coloreOcchi;
     }
 
+    /**
+     * Imposta il colore degli occhi del personaggio.
+     * @param coloreOcchi Il nuovo colore degli occhi del personaggio.
+     */
     @Override
-    public void setColoreOcchi(String coloreOcchi) {
+    public void setColoreOcchi(final String coloreOcchi) {
         this.coloreOcchi = coloreOcchi;
     }
 
+    /**
+     * Verifica se il personaggio ha la barba.
+     * @return True se il personaggio ha la barba, altrimenti False.
+     */
     @Override
     public Boolean getBarba() {
         return barba;
     }
 
+    /**
+     * Imposta se il personaggio ha la barba.
+     * @param barba True se il personaggio ha la barba, altrimenti False.
+     */
     @Override
-    public void setBarba(Boolean barba) {
+    public void setBarba(final Boolean barba) {
         this.barba = barba;
     }
 
+    /**
+     * Verifica se il personaggio indossa gli occhiali.
+     * @return True se il personaggio indossa gli occhiali, altrimenti False.
+     */
     @Override
     public Boolean hasOcchiali() {
         return occhiali;
     }
 
+    /**
+     * Imposta se il personaggio indossa gli occhiali.
+     * @param occhiali True se il personaggio indossa gli occhiali, altrimenti False.
+     */
     @Override
-    public void setOcchiali(Boolean occhiali) {
+    public void setOcchiali(final Boolean occhiali) {
         this.occhiali = occhiali;
     }
 
+    /**
+     * Verifica se il personaggio ha accessori.
+     * @return True se il personaggio ha accessori, altrimenti False.
+     */
     @Override
     public Boolean hasAccessori() {
         return accessorio;
     }
 
+    /**
+     * Imposta se il personaggio ha accessori.
+     * @param accessorio True se il personaggio ha accessori, altrimenti False.
+     */
     @Override
-    public void setAccessori(Boolean accessorio) {
+    public void setAccessori(final Boolean accessorio) {
         this.accessorio = accessorio;
     }
 
+    /**
+     * Verifica se il personaggio ha i baffi.
+     * @return True se il personaggio ha i baffi, altrimenti False.
+     */
     @Override
     public Boolean hasBaffi() {
         return baffi;
     }
 
+    /**
+     * Imposta se il personaggio ha i baffi.
+     * @param baffi True se il personaggio ha i baffi, altrimenti False.
+     */
     @Override
-    public void setBaffi(Boolean baffi) {
+    public void setBaffi(final Boolean baffi) {
         this.baffi = baffi;
     }
 
+    /**
+     * Verifica se il personaggio è di sesso maschile.
+     * @return True se il personaggio è di sesso maschile, altrimenti False.
+     */
     @Override
     public Boolean isUomo() {
         return uomo;
     }
 
+    /**
+     * Imposta se il personaggio è di sesso maschile.
+     * @param uomo True se il personaggio è di sesso maschile, altrimenti False.
+     */
     @Override
-    public void setUomo(Boolean uomo) {
+    public void setUomo(final Boolean uomo) {
         this.uomo = uomo;
     }
 }
