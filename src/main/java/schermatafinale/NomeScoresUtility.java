85094f2fc7d92657b8752ec67acff58ffd882085
Alex Petreti <alex.petreti@studio.unibo.it>
2024-02-18 16:01:39 +0100

    corretti alcuni errori

--- src/main/java/schermatafinale/NomeScoresUtility.java
+++ src/main/java/schermatafinale/NomeScoresUtility.java
@@ -1,7 +1,13 @@
 package schermatafinale;
 
-import java.io.*;
-import java.util.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
 
 public class NomeScoresUtility {
 
