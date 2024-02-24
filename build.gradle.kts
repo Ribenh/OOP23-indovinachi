6a1225b1e6f2d3e0148789ab86a9ee977a8ebb84
Alex Petreti <alex.petreti@studio.unibo.it>
2024-02-24 17:29:24 +0100

    modifica alle proprietà per jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
 
 application {
     // Define the main class for the application.
-    mainClass.set("it.unibo.sampleapp.RateAMovie")
+    mainClass.set("src.main.java.schermatainiziale.SchermataInizialeGUI")
 }
 
 tasks.test {
